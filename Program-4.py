2c178a306c96c3c756be57d55b40efaec582d8d6
Shai_Base <shaibase1328@gmail.com>
2024-01-08 14:02:11 +0800

    Created a window

---
+++ Program-4.py
@@ -0,0 +1,16 @@
+# Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
+# - Created new github repository
+# - Upload your programs to github using git bash.
+# - Record a demo presenting your programs (less than 2 min only)
+# - Send the demo directly to my messenger
+# - Deadline before Jan 13, 2024
+
+import tkinter
+
+app = tkinter.Tk()
+app.title("Ship Shop")
+app.geometry('700x400')
+app.resizable(0, 0)
+
+
+app.mainloop()
