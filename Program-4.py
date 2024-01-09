ac0900747f798e9a1019fcfdddcdc06a36a68bba
Shai_Base <shaibase1328@gmail.com>
2024-01-09 13:23:06 +0800

    Added background

--- Program-4.py
+++ Program-4.py
@@ -5,12 +5,40 @@
 # - Send the demo directly to my messenger
 # - Deadline before Jan 13, 2024
 
-import tkinter
+# import packages
+import customtkinter
+import customtkinter as ctk
+from tkinter import *
+from tkinter import messagebox
+from customtkinter import *
+from PIL import Image
 
-app = tkinter.Tk()
-app.title("Ship Shop")
-app.geometry('700x400')
-app.resizable(0, 0)
+# set theme
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme('green')
+
+# create window
+input_window = ctk.CTk()
+input_window.title("Ship Shop")
+input_window.geometry('350x400')
+input_window.resizable(0, 0)
+
+# add images
+auction_background_data = Image.open("auction_bg.png")
+auction_background = CTkImage(dark_image=auction_background_data,
+                              light_image=auction_background_data,
+                              size=(350, 400))
+
+# background
+window_frame = CTkFrame(input_window, width=700, height=400)
+window_frame.pack()
+
+window_label = CTkLabel(window_frame, text='', image=auction_background, width=350, height=400)
+window_label.pack(fill='both', expand=True)
+
+# entries
 
 
-app.mainloop()
+
+
+input_window.mainloop()
