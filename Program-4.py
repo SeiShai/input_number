2532e23883c4b9db614406b0082e7b1916ca848f
Shai_Base <shaibase1328@gmail.com>
2024-01-09 16:21:47 +0800

    Edited the layout

--- Program-4.py
+++ Program-4.py
@@ -1,6 +1,6 @@
 # Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
-# - Created new github repository
-# - Upload your programs to github using git bash.
+# - Created new gitHub repository
+# - Upload your programs to gitHub using git bash.
 # - Record a demo presenting your programs (less than 2 min only)
 # - Send the demo directly to my messenger
 # - Deadline before Jan 13, 2024
@@ -20,21 +20,28 @@ customtkinter.set_default_color_theme('green')
 # create window
 input_window = ctk.CTk()
 input_window.title("Ship Shop")
-input_window.geometry('350x400')
+input_window.geometry('660x400')
 input_window.resizable(0, 0)
 
 # add images
 auction_background_data = Image.open("auction_bg.png")
 auction_background = CTkImage(dark_image=auction_background_data,
                               light_image=auction_background_data,
-                              size=(350, 400))
+                              size=(330, 400))
+
+input_background_data = Image.open("input_bg.png")
+input_background = CTkImage(dark_image=input_background_data,
+                              light_image=input_background_data,
+                              size=(330, 400))
 
 # background
-window_frame = CTkFrame(input_window, width=700, height=400)
+window_frame = CTkFrame(input_window, width=660, height=400)
 window_frame.pack()
 
-window_label = CTkLabel(window_frame, text='', image=auction_background, width=350, height=400)
-window_label.pack(fill='both', expand=True)
+CTkLabel(window_frame, text="", image=auction_background).pack(expand=True, side="right")
+CTkLabel(window_frame, text="", image=input_background).pack(expand=True, side="left")
+
+
 
 # entries
 
